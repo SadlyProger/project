59d9a9b2b5a7e797ed30fd36fb0b898fc21486b8
Andrey <alik610@gmail.com>
2022-09-07 10:34:15 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+'use strict';
+
+a = 5;
+console.log(a);
+
+
+let persone = {
+  name: "John",
+  age: 25,
+  isMarried: false,
+};
+console.log(persone.name);
+console.log(persone["name"]);
+
+let arr = ["plum.png", "orange.jpeg", "apple.bmp"];
+console.log(arr[2]);
+
+// alert("HelloWorld");
+
+// let answer = confirm("Are u here?");
+// console.log(answer);
+
+// let answer = +prompt("Есть ли вам 18?", "");
+// console.log(typeof(answer));
+
+// console.log("arr" + " - object");
+// console.log(4 + +" - object");
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" === 2);
+
+let isChecked = true,
+    isClose = false;
+
+console.log(isChecked || isClose);
+
+
+
+
+
+
+
+
